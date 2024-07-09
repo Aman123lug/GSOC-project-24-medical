8467053dd8ae2773eb2cd04034d2ad6190e5b18f
Aman123lug <ak06465676@gmail.com>
2024-07-10 01:26:15 +0530

    updated requirements.txt

--- features/scan_medicines.py
+++ features/scan_medicines.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pytesseract
+from PIL import Image
 
 st.title("testing features......")
 
@@ -6,3 +8,15 @@ picture = st.camera_input("Take a picture")
 
 if picture:
     st.image(picture)
+
+# If you installed Tesseract OCR using the installer on Windows, you might need to specify the path to the executable
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Load an image from file
+image = Image.open('features/sample_images_scan/crotamiton-hydrocortisone-cream.jpeg')
+
+# Use pytesseract to do OCR on the image
+text = pytesseract.image_to_string(image)
+
+print(text)
+st.write(text)
